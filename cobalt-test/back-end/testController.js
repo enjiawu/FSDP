9bdd376ec9871bca13bcb87f897ab8085a19f2de
Jayme-Chua <s10257137@connect.np.edu.sg>
2024-11-15 00:07:41 +0800

    Check user for browsers (INCOMPLETE)

--- cobalt-test/back-end/testController.js
+++ cobalt-test/back-end/testController.js
@@ -1,9 +1,21 @@
 const { exec } = require('child_process');
 const browsers = ['chrome', 'firefox', 'edge', 'opera']
 const testFolder = '../back-end/test_cases';
+//const checkBrowserAvailability = require('./browserController');
 
 const runTest = (req, res) => {
-    exec(`testcafe ${browsers} ${testFolder} -- concurrently 2`, (error, stdout, stderr) => {
+    /*
+    const { continueWithAvailable } = req.body;
+    const { availableBrowsers, missingBrowsers } = checkBrowserAvailability();
+    if (missingBrowsers.length > 0 && !continueWithAvailable) {
+        return res.status(500).json({
+            message: 'Some browsers are missing',
+            missingBrowsers
+        });
+    }
+    const browsersToUse = availableBrowsers.join(',');
+    */
+    exec(`testcafe ${browsers} ${testFolder} -- concurrency 2`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing test: ${error.message}`);
             return res.status(500).json({ message: 'Test execution failed', error: error.message });
@@ -20,9 +32,21 @@ const runTest = (req, res) => {
 };
 
 const runSelectedTest = (req, res) => {
+    /*
+    const { testCases, continueWithAvailable } = req.body;
+    const { availableBrowsers, missingBrowsers } = checkBrowserAvailability();
+
+    if (missingBrowsers.length > 0 && !continueWithAvailable) {
+        return res.status(500).json({
+            message: 'Some browsers are missing',
+            missingBrowsers
+        });
+    }
+    const browsersToUse = availableBrowsers.join(',');
+    */
     const { testCases } = req.body;
     const testCaseFiles = testCases.map(testCase => `${testFolder}/${testCase}`).join(' ');
-    exec(`testcafe ${browsers} ${testCaseFiles} -- concurrently 2`, (error, stdout, stderr) => {
+    exec(`testcafe ${browsers} ${testCaseFiles} -- concurrency 2`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing test: ${error.message}`);
             return res.status(500).json({ message: 'Test execution failed', error: error.message });
